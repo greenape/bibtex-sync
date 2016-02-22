59dceba0207f61da7b743b03a5705e166049091c
Jonathan Gray <j.gray@soton.ac.uk>
2016-02-22 15:16:25 +0000

    Added an option to fix the stupid mendeley annote field

--- bibtex.py
+++ bibtex.py
@@ -16,6 +16,8 @@ def arguments():
     			   default=[],
                    help='Fields to strip.',
                    dest="fields")
+    parser.add_argument('--fix-note', dest='fix_note', action='store_const',
+    	const=True, default=False, help="Switch Mendeley annote to note. (default: False")
     args = parser.parse_args()
     return args
 
@@ -26,12 +28,15 @@ def strip_field(field_name, bibtex):
 	reg = "\n%s.*,\n" % field_name
 	return re.sub(reg, "\n", bibtex)
 
-def do_sync(in_file, out_dir, strip_fields):
+def do_sync(in_file, out_dir, strip_fields, fix_note):
 	path, fname = os.path.split(in_file)
 	with open(in_file, "r") as f:
 		bibtex = f.read()
 	for field in strip_fields:
 		bibtex = strip_field(field, bibtex)
+	if fix_note:
+		reg = "\nannote ="
+		bibtex = re.sub(reg, "\nnote =", bibtex)
 	fname = fname.replace(" ", "_")
 	target = os.path.join(out_dir, fname)
 	with open(target, "w") as f:
@@ -41,6 +46,6 @@ if __name__ == "__main__":
 	args = arguments()
 	print "Copying %s to %s." % (args.infile[0], args.outdir[0])
 	print "Removing fields: " + " ".join(args.fields)
-	do_sync(args.infile[0], args.outdir[0], args.fields)
+	do_sync(args.infile[0], args.outdir[0], args.fields, args.fix_note)
 	print 'Done.'
 
